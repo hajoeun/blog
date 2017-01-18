ec7d8d78cb6596896bb91baae5433146afa2715b
Joeun <imjoeunha@gmail.com>
2017-01-19 00:02:27 +0900

    Create: First Scroll Controll Button

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,1 +1,32 @@
-console.log("hello joeun");
+$(function() {
+  var $nextBtn = $('.next.btn'), $section = $('section'), $body = $('body');
+  $body.stop().animate({scrollTop: 0})
+  
+  $body.on({
+    'mousewheel': function(e) {
+//      if ($nextBtn.attr('id') == $section.length - 1) return;
+      e.preventDefault();
+      e.stopPropagation();
+    }
+  })
+  
+  
+  $nextBtn.on('click', function(e) {
+    var nextId = parseInt(e.currentTarget.id) + 1
+        length = $section.length - 1;
+    
+    if (nextId > length) return;
+    
+    $nextBtn.attr('id', nextId);
+
+    
+    $body.stop().animate({
+      scrollTop: $section.eq(nextId).offset().top
+    }, 1500)
+    
+
+    if (nextId === length) {
+      $nextBtn.text("NEXT");
+    }
+  })
+})
