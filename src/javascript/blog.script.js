cf4912a87a374c35732654e306a0bf1fe4d58e84
Joeun <imjoeunha@gmail.com>
2017-01-26 00:00:45 +0900

    Update: blog link
    - 마지막 페이지의 블로그 링크 태그 수정
    - 애니메이션 효과 삽입

---
+++ src/javascript/blog.script.js
@@ -0,0 +1,20 @@
+  // // Initialize Firebase
+  // var config = {
+  //   apiKey: "AIzaSyBBj6nL4bI6HRCRUUxWiVnnE1RxTvvZSWQ",
+  //   authDomain: "firstfirebase-7edfd.firebaseapp.com",
+  //   databaseURL: "https://firstfirebase-7edfd.firebaseio.com",
+  //   storageBucket: "firstfirebase-7edfd.appspot.com",
+  //   messagingSenderId: "184350321647"
+  // };
+  // firebase.initializeApp(config);
+
+  // var database = firebase.database();
+  // var auth = firebase.auth();
+  
+  // auth.signInAnonymously().catch(function(error) {
+  //   // Handle Errors here.
+  //   var errorCode = error.code;
+  //   var errorMessage = error.message;
+  //   alert(errorCode, errorMessage);
+  //   // ...
+  // });
--- src/javascript/index.script.js
+++ src/javascript/index.script.js
@@ -1,25 +1,4 @@
 $(function() {
-  // // Initialize Firebase
-  // var config = {
-  //   apiKey: "AIzaSyBBj6nL4bI6HRCRUUxWiVnnE1RxTvvZSWQ",
-  //   authDomain: "firstfirebase-7edfd.firebaseapp.com",
-  //   databaseURL: "https://firstfirebase-7edfd.firebaseio.com",
-  //   storageBucket: "firstfirebase-7edfd.appspot.com",
-  //   messagingSenderId: "184350321647"
-  // };
-  // firebase.initializeApp(config);
-
-  // var database = firebase.database();
-  // var auth = firebase.auth();
-  
-  // auth.signInAnonymously().catch(function(error) {
-  //   // Handle Errors here.
-  //   var errorCode = error.code;
-  //   var errorMessage = error.message;
-  //   alert(errorCode, errorMessage);
-  //   // ...
-  // });
-
   var $nextBtn = $('.next.btn'), 
       $prevBtn = $('.prev.btn'),
       $section = $('section'), 
@@ -76,7 +55,7 @@ $(function() {
       }
       if (pages.current === length) {
         // $('.btn.next').hide();
-        $('.btn.next').html("<a href='blog.html'>확인하기</a>")
+        $('i.icon-angle-down').replaceWith("<a href='http://joeun.me'><strong>joeun.me</strong>에서<br/>확인하기</a>")
       }
     })
   }
