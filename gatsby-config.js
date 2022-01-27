037bbdbb9b460d5c9e444d1f792e61835a8557db
하조은 <hajoeun90@gmail.com>
2022-01-27 21:38:42 +0900

    Replace blog url (#32)
    * 블로그 주소 변경
    
    * 파비콘, 사이트 이미지 수정
    
    * description 수정
    
    * 블로그 이름 수정
    
    * 블로그 소개 문구 수정

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,13 +6,13 @@ const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE
 
 module.exports = {
   siteMetadata: {
-    siteTitle: `하조은의 개발 블로그`,
-    siteTitleAlt: `하조은의 개발 블로그`,
-    siteHeadline: `하조은의 개발 블로그`,
-    siteUrl: `https://joeun.dev`,
+    siteTitle: `하조은의 블로그`,
+    siteTitleAlt: `하조은의 블로그`,
+    siteHeadline: `하조은의 블로그`,
+    siteUrl: `https://hajoeun.blog/`,
     siteImage: `/site-image.png`,
     siteLanguage: `ko`,
-    siteDescription: `스타트업, 개발자, 블로그, 하조은, 소프트웨어, 엔지니어, blog, developer, engineer, hajoeun, joeun`,
+    siteDescription: `스타트업, 개발자, 블로그, 하조은, 소프트웨어, 엔지니어, blog, developer, engineer, hajoeun, hajoeun.blog`,
     author: `@hajoeun`
   },
   plugins: [
@@ -40,7 +40,7 @@ module.exports = {
           },
         ],
         feed: true,
-        feedTitle: `하조은의 개발 블로그`,
+        feedTitle: `하조은의 블로그`,
         formatString: 'YYYY.MM.DD',
       },
     },
@@ -54,9 +54,9 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `하조은의 개발 블로그`,
-        short_name: `하조은의 개발 블로그`,
-        description: `스타트업, 개발자, 블로그, 하조은, 소프트웨어, 엔지니어, blog, developer, engineer, hajoeun, joeun.dev`,
+        name: `하조은의 블로그`,
+        short_name: `하조은의 블로그`,
+        description: `스타트업, 개발자, 블로그, 하조은, 소프트웨어, 엔지니어, blog, developer, engineer, hajoeun, hajoeun.blog`,
         start_url: `/`,
         background_color: `#fff`,
         theme_color: `#6B46C1`,
