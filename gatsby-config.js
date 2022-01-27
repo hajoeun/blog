7701ff06eb0c533d0c0c6ead5305e9b38b7c638f
hajoeun <hajoeun90@gmail.com>
2022-01-27 21:31:53 +0900

    블로그 이름 수정

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,9 +6,9 @@ const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE
 
 module.exports = {
   siteMetadata: {
-    siteTitle: `하조은의 개발 블로그`,
-    siteTitleAlt: `하조은의 개발 블로그`,
-    siteHeadline: `하조은의 개발 블로그`,
+    siteTitle: `하조은의 블로그`,
+    siteTitleAlt: `하조은의 블로그`,
+    siteHeadline: `하조은의 블로그`,
     siteUrl: `https://hajoeun.blog/`,
     siteImage: `/site-image.png`,
     siteLanguage: `ko`,
@@ -40,7 +40,7 @@ module.exports = {
           },
         ],
         feed: true,
-        feedTitle: `하조은의 개발 블로그`,
+        feedTitle: `하조은의 블로그`,
         formatString: 'YYYY.MM.DD',
       },
     },
@@ -54,8 +54,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `하조은의 개발 블로그`,
-        short_name: `하조은의 개발 블로그`,
+        name: `하조은의 블로그`,
+        short_name: `하조은의 블로그`,
         description: `스타트업, 개발자, 블로그, 하조은, 소프트웨어, 엔지니어, blog, developer, engineer, hajoeun, hajoeun.blog`,
         start_url: `/`,
         background_color: `#fff`,
