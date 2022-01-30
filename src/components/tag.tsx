bc915d66c9c44f1ea4014329a7964ca753568c2f
하조은 <hajoeun90@gmail.com>
2022-01-30 11:22:27 +0900

    Remove "View all tags" link (#39)
    * View all tags 링크 제거
    
    * 페이지 높이값 조정

--- src/components/tag.tsx
+++ src/components/tag.tsx
@@ -35,12 +35,7 @@ const Tag = ({ posts, pageContext }: TagProps) => {
   return (
     <Layout>
       <SEO title={`Tag: ${pageContext.name}`} />
-      <Flex sx={{ alignItems: `center`, justifyContent: `space-between`, flexFlow: `wrap` }}>
-        <Heading variant="styles.h2">{pageContext.name}</Heading>
-        <TLink as={Link} sx={{ variant: `links.secondary` }} to={replaceSlashes(`/${basePath}/${tagsPath}`)}>
-          View all tags
-        </TLink>
-      </Flex>
+      <Heading variant="styles.h2">{`Tag: ${pageContext.name}`}</Heading>
       <Listing posts={posts} sx={{ mt: [4, 5] }} />
     </Layout>
   )
