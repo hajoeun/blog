0fc223df66ee38e78fe2f36191062bd8b9c1fd85
Ha Joeun <hajoeun90@gmail.com>
2024-07-21 20:20:32 +0900

    update - dynamic og image (#120)
    * copy leerob.io
    
    * copy guillermo
    
    * prebuild for posts
    
    * update og images
    
    * fix og image
    
    * delete font
    
    * update metadata in 2024
    
    * fix og url

---
+++ app/og/[id]/route.tsx
@@ -0,0 +1,48 @@
+import { ImageResponse } from 'next/og';
+
+export const runtime = 'edge';
+
+export async function GET(_, { params: { id } }) {
+  const posts = await fetch(new URL('../../../src/databases/posts.json', import.meta.url)).then(
+    (res) => res.json()
+  );
+  const post = posts.find((post) => post.id === id);
+
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          height: '100%',
+          width: '100%',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'flex-start',
+          justifyContent: 'center',
+          backgroundImage: 'url(https://hajoeun.com/og-background.png)',
+          backgroundColor: '#242424',
+        }}
+      >
+        <div
+          style={{
+            marginLeft: 120,
+            marginRight: 190,
+            paddingBottom: 190,
+            display: 'flex',
+            fontSize: 130,
+            letterSpacing: '-0.05em',
+            fontStyle: 'normal',
+            color: '#fefcfc',
+            lineHeight: '180px',
+            whiteSpace: 'pre-wrap',
+          }}
+        >
+          {post.title}
+        </div>
+      </div>
+    ),
+    {
+      width: 1920,
+      height: 1080,
+    }
+  );
+}
