d218eaca6bba3bdf30d7a8fbd270b012eb0e6d3c
hajoeun <imjoeunha@gmail.com>
2025-12-28 20:49:56 +0900

    remove - course link

--- src/components/main-header.tsx
+++ src/components/main-header.tsx
@@ -13,12 +13,6 @@ export function MainHeader() {
       <nav className="font-mono text-xs grow justify-end items-center flex gap-1 md:gap-3">
         <ThemeToggle />
         <HeaderLink />
-        <Link
-          href="https://book.hajoeun.dev/friendly-next-js"
-          className="inline-flex hover:bg-gray-200 dark:hover:bg-[#313131] active:bg-gray-300 dark:active:bg-[#242424] rounded-sm p-2 transition-[background-color]"
-        >
-          Course
-        </Link>
       </nav>
     </header>
   );
